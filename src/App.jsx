7aa31396da8e20035a8b0fac3454a69528be3e2f
PRINCEJAGAPATHI <134799214+PRINCEJAGAPATHI@users.noreply.github.com>
2023-07-03 21:34:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  
+  return (
+    <>
+    <Header/>
+    <Filter/>
+   <MainData/>
+   <MainData1/>
+    </>
+  )
+}
+
+export default App
